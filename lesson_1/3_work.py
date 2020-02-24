663ea42a69f38958ca0af64fa77706896278a2c4
Igor.G <jemper12@gmail.com>
2020-02-24 22:01:33 +0200

    lessons one, work 3

---
+++ lesson_1/3_work.py
@@ -0,0 +1,14 @@
+def test_num(num):
+    if not num % 15:
+        print('FizzBuzz')
+    elif not num % 5:
+        print('Buzz')
+    elif not num % 3:
+        print('Fizz')
+    else:
+        print(num)
+
+
+for number in range(101):
+    if number > 0:
+        test_num(number)
