e396fd58b3baf847bb3e94a1eca6e90b9e4457dd
Igor.G <jemper12@gmail.com>
2020-02-27 18:45:39 +0200

    add comment

--- lessons_1/1_task.py
+++ lessons_1/1_task.py
@@ -1,3 +1,7 @@
+'''
+Создать список из N элементов (от 0 до n с шагом 1). В этом списке вывести все четные значения.
+'''
+
 n = input('input number')
 list_number = list(range(int(n)))
 for num in list_number:
