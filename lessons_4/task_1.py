4ce1b2cc2e63d60b04b40d299ab8afbeca0a6d95
Igor.G <jemper12@gmail.com>
2020-03-12 17:36:29 +0200

    complete task 1

--- lessons_4/task_1.py
+++ lessons_4/task_1.py
@@ -14,13 +14,16 @@
 from abc import ABC, abstractmethod
 from datetime import datetime
 
+person_list = []
+
 
 class Person(ABC):
 
     def __init__(self, second_name, birthday, faculty):
         self._second_name = second_name
-        self._birthday = datetime.now().year - datetime(birthday[0], birthday[1], birthday[2])
+        self._age = datetime.now().year - datetime(birthday[0], birthday[1], birthday[2]).year
         self._faculty = faculty
+        self._birthday = f'{birthday[0]}/{birthday[1]}/{birthday[2]}'
 
     @abstractmethod
     def info(self):
@@ -37,14 +40,69 @@ class Entrant(Person):
 
     @property
     def info(self):
-        return self._second_name
+        a = f"""
+second name = {self._second_name}
+birthday = {self._birthday}
+faculty = {self._faculty}"""
+        return a
 
     @property
     def age(self):
-        return self._birthday
+        return self._age
 
 
-person1 = Entrant('Gaevuy', (1994, 11, 12), 'Engine')
+class Student(Person):
+    def __init__(self, second_name, birthday, faculty, course):
+        super().__init__(second_name, birthday, faculty)
+        self._course = course
 
-print(person1.info)
-print(person1.age)
+    @property
+    def info(self):
+        a = f"""
+second name = {self._second_name}
+birthday = {self._birthday}
+faculty = {self._faculty}
+course = {self._course}
+"""
+        return a
+
+    @property
+    def age(self):
+        return self._age
+
+
+class Teacher(Person):
+    def __init__(self, second_name, birthday, faculty, position, experience):
+        super().__init__(second_name, birthday, faculty)
+        self._position = position
+        self._experience = experience
+
+    @property
+    def info(self):
+        a = f"""
+second name = {self._second_name}
+birthday = {self._birthday}
+faculty = {self._faculty}
+position = {self._position}
+experience = {self._experience}
+"""
+        return a
+
+    @property
+    def age(self):
+        return self._age
+
+
+person_list.append(Entrant('Gaevuy', (1994, 6, 7), 'Engine'))
+person_list.append(Entrant('Galofastov', (1993, 4, 25), 'Engine'))
+person_list.append(Student('Ivanov', (1990, 12, 12), 'Engine', 4))
+person_list.append(Teacher('Golovan', (1975, 11, 19), 'Engine', 'teacher', 15))
+
+for person in person_list:
+    print(person.info)
+
+print('search for a person with an age of 25 years')
+
+for person in person_list:
+    if 20 < person.age < 27:
+        print(person.info, person.age)
