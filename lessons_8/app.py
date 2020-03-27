fa601fb36484601ce41914c0cb5a259f37df3f83
Igor.G <jemper12@gmail.com>
2020-03-27 16:19:20 +0200

    complete 8 lessons

--- lessons_8/app.py
+++ lessons_8/app.py
@@ -1,41 +1,93 @@
-from flask import Flask
-from flask import render_template
-from .db import POST
+from flask import Flask, render_template, request, redirect, url_for
+from db import ContexSqlManager, sql_all_cetegory, sql_select_current_category, sql_select_product, \
+    sql_select_all_status, sql_inser_new_category, sql_inser_new_product
 
 app = Flask(__name__)
 
 """
 MVC
 """
+""""
+Создать базу данных товаров, у товара есть:
+    Категория (связанная таблица),
+    название, есть ли товар в продаже или на складе, цена, кол-во единиц.Создать html страницу. 
+На первой странице выводить ссылки на все категории,
+    при переходе на категорию получать список всех товаров в наличии ссылками, при клике на товар выводить его цену, 
+    полное описание и кол-во единиц в наличии.
+"""
+
+DB = 'database.sqlite'
 
 
 @app.route('/')
 def index():
-    page_title = 'My first page'
-    name = ['MyPage', 'hello']
-    return render_template('index.html', page_title=page_title, name=name)
+    with ContexSqlManager(DB) as db:
+        exec_ = db.my_select(sql_all_cetegory)
+        data = {}
+        for to_data in exec_:
+            data[to_data[0]] = to_data[1]
+
+    return render_template('index.html', data=data)
 
 
-@app.route('/posts')
-def posts():
-    return render_template('posts.html', posts=POST)
+@app.route('/category/<category_id>')
+def category(category_id):
+    with ContexSqlManager(DB) as db:
+        exec_ = db.my_select(sql_select_current_category, category_id)
+        data = {}
+        for to_data in exec_:
+            data[to_data[0]] = to_data[1]
+
+    return render_template('category.html', data=data)
 
 
-@app.route('/post/')
-@app.route('/post/<int:post_id>')
-def post(post_id=None):
+@app.route('/product/<product_id>')
+def product(product_id):
+    with ContexSqlManager(DB) as db:
+        exec_ = db.my_select(sql_select_product, product_id)[0]
+        name = exec_[0]
+        cost = exec_[1]
+        quantity = exec_[2]
+        status = exec_[3]
+        category_name = exec_[4]
+        category_id = exec_[5]
+        description = exec_[6]
+    return render_template('product.html', name=name, cost=cost, quantity=quantity, status=status,
+                           category_name=category_name, category_id=category_id, description=description)
 
-    return render_template('post_body.html', post_body=POST[post_id-1])
 
+@app.route('/admin', methods=['POST', 'GET'])
+def admin():
+    if request.method == 'GET':
+        category_dict = {}
+        status_dict = {}
 
-@app.route('/hi')
-def hola():
-    names = ['Igor',
-             'Anton',
-             'Max',
-             'Ann',
-             'Luiz']
-    return render_template('hi.html', names=names)
+        with ContexSqlManager(DB) as db:
+
+            category_list = db.my_select(sql_all_cetegory)
+            for data in category_list:
+                category_dict[data[0]] = data[1]
+
+            status_list = db.my_select(sql_select_all_status)
+            for data in status_list:
+                status_dict[data[0]] = data[1]
+
+        return render_template('admin.html', status_dict=status_dict, category_dict=category_dict)
+
+    elif request.method == 'POST':
+        if 'category_name' in request.form:
+            if request.form['category_name'] != '':
+                with ContexSqlManager(DB) as db:
+                    db.my_execute(sql_inser_new_category, request.form['category_name'])
+        else:
+            if request.form['product_name'] != '' and request.form['cost'] != '' and \
+                    request.form['quantity'] != '' and request.form['description'] != '':
+                with ContexSqlManager(DB) as db:
+                    # product_name, status_id, cost, quantity, category_id, description
+                    db.my_execute(sql_inser_new_product, request.form['product_name'], request.form['status'],
+                                  request.form['cost'], request.form['quantity'], request.form['category'],
+                                  request.form['description'])
+        return redirect(url_for('admin'))
 
 
 if __name__ == '__main__':
--- lessons_8/db.py
+++ lessons_8/db.py
@@ -1,19 +1,64 @@
-POST = [
-    {
-        'id': 1,
-        'title': 'Coronavirus',
-        'body': 'More text',
-        'author': 'Igor'
-    },
-    {'id': 2,
-     'title': 'Baracuda',
-     'body': 'More text text baracuda',
-     'author': 'Vanya'
-     }
-    ,
-    {'id': 3,
-     'title': 'Constantu',
-     'body': 'More text text baracuda Constanta',
-     'author': 'Vanya'
-     }
-]
+import sqlite3
+
+
+class ContexSqlManager:
+    def __init__(self, name):
+        self._name = name
+
+    def __enter__(self):
+        self._conn = sqlite3.connect(self._name)
+        return self
+
+    def my_execute(self, sql, *args):
+        cursor = self._conn.cursor()
+        cursor.execute(sql, args)
+        self._conn.commit()
+        return cursor.fetchall()
+
+    def my_select(self, sql, *args):
+        cursor = self._conn.cursor()
+        cursor.execute(sql, args)
+        return cursor.fetchall()
+
+    def __exit__(self, *args):
+        self._conn.commit()
+        self._conn.close()
+
+
+sql_all_cetegory = """
+select id, category_name from category
+"""
+
+sql_select_current_category = """
+select id, product_name
+from product
+where status_id != 3
+  and category_id = ?"""
+
+sql_select_product = """
+select product_name,
+       cost,
+       quantity,
+       s.name as status,
+       c.category_name as category,
+       c.id as category_id,
+       description
+from product
+join category c on product.category_id = c.id
+join status s on product.status_id = s.id
+where product.id = ?
+"""
+
+sql_select_all_status = """
+select id, name from status
+"""
+
+sql_inser_new_category = """
+insert into category (category_name)
+values (?)
+"""
+
+sql_inser_new_product = """
+insert into product (product_name, status_id, cost, quantity, category_id, description)
+values (?,?,?,?,?,?)
+"""
